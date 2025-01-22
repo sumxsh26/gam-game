c2da097dc298159191554cda2ac74dc7cace88a5
sumxsh26 <smyyhzaii@gmail.com>
2025-01-22 20:53:31 +0800

    fixed falling object stucj

--- Assets/FallingSpikes/FallingSpikesController.cs
+++ Assets/FallingSpikes/FallingSpikesController.cs
@@ -1,20 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class FallingSpikesController : MonoBehaviour
+public class ObjectFallController : MonoBehaviour
 {
     float wait = 0.1f;
-    public GameObject fallingSpike;
+    public GameObject fallingObject;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called before the first frame update
     void Start()
     {
-        // to make the fall repeat, wait is time btw dropping objects
         InvokeRepeating("Fall", wait, wait);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void Fall()
     {
-        Instantiate(fallingSpike, new Vector3(Random.Range(-10, 10), 10, 0), Quaternion.identity);
+        Instantiate(fallingObject, new Vector3(Random.Range(-10, 10), 10, 0), Quaternion.identity);
     }
 }

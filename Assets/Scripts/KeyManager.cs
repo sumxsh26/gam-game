1c826dc1680262d8079eb08a8f1df97d23931896
joann <j29bisseling@gmail.com>
2025-02-11 15:26:03 +0800

    alpha scene - jo

--- Assets/Scripts/KeyManager.cs
+++ Assets/Scripts/KeyManager.cs
@@ -5,7 +5,6 @@ public class KeyManager : MonoBehaviour
     public int keyCount = 0;
     public int maxKeys = 3;
     public GameObject door;
-    public GameObject secondDoor;
     private bool doorDestroyed = false;
 
     public KeyDisplay keyDisplay;  // Direct reference to KeyDisplay
@@ -16,7 +15,6 @@ public class KeyManager : MonoBehaviour
         {
             doorDestroyed = true;
             Destroy(door);
-            Destroy(secondDoor);
         }
     }
 

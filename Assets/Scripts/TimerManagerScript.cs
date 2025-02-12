3b7c971c17725b949c612d5ec1e4edf1a0c6ec85
joann <j29bisseling@gmail.com>
2025-02-12 14:32:40 +0800

    recording alpha

--- Assets/Scripts/TimerManagerScript.cs
+++ Assets/Scripts/TimerManagerScript.cs
@@ -1,10 +1,51 @@
+//using TMPro;
+//using UnityEngine;
+
+//public class TimerManagerScript : MonoBehaviour
+//{
+//    [SerializeField] TextMeshProUGUI countdownTimerText;
+//    [SerializeField] float remainingTime;
+
+//    void Update()
+//    {
+//        if (GameControllerScript.isGameOver)
+//            return; // Stop updating timer if game over
+
+//        if (remainingTime > 0)
+//        {
+//            remainingTime -= Time.deltaTime;
+//        }
+//        else if (remainingTime < 0)
+//        {
+//            remainingTime = 0;
+//            countdownTimerText.color = Color.red;
+
+//            // Trigger the PlayerDied event safely
+//            GameControllerScript.Instance?.PlayerController?.TriggerPlayerDeath();
+//        }
+
+//        int minutes = Mathf.FloorToInt(remainingTime / 60);
+//        int seconds = Mathf.FloorToInt(remainingTime % 60);
+//        countdownTimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+//    }
+//}
+
 using TMPro;
 using UnityEngine;
 
 public class TimerManagerScript : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI countdownTimerText;
-    [SerializeField] float remainingTime;
+    [SerializeField] float remainingTime = 300f;  // Set a default starting time (e.g., 5 minutes)
+
+    void Start()
+    {
+        // If the remainingTime is set to zero or an invalid value, reset it to the default
+        if (remainingTime <= 0)
+        {
+            remainingTime = 300f; // Set to a default value, e.g., 5 minutes
+        }
+    }
 
     void Update()
     {
@@ -15,7 +56,7 @@ public class TimerManagerScript : MonoBehaviour
         {
             remainingTime -= Time.deltaTime;
         }
-        else if (remainingTime < 0)
+        else if (remainingTime <= 0)
         {
             remainingTime = 0;
             countdownTimerText.color = Color.red;
@@ -29,3 +70,4 @@ public class TimerManagerScript : MonoBehaviour
         countdownTimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
+

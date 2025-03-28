817fd22d7e645e461e1fa4a0f2038dc6d3fed01e
joann <j29bisseling@gmail.com>
2025-03-29 01:06:56 +0800

    updates

--- Assets/Scripts/Managers/RestartController.cs
+++ Assets/Scripts/Managers/RestartController.cs
@@ -1,3 +1,29 @@
+////using UnityEngine;
+////using UnityEngine.SceneManagement;
+
+////public class RestartController : MonoBehaviour
+////{
+////    void Update()
+////    {
+////        if (InputManager.RestartWasPressed)
+////        {
+////            FullRestart();
+////        }
+////    }
+
+////    private void FullRestart()
+////    {
+////        // Clear checkpoint before reloading
+////        if (CheckpointManager.Instance != null)
+////        {
+////            CheckpointManager.Instance.ClearCheckpointData();
+////        }
+
+////        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+////    }
+////}
+
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -13,12 +39,20 @@ public class RestartController : MonoBehaviour
 
     private void FullRestart()
     {
-        // Clear checkpoint before reloading
+        // Hide the Game Over canvas before reloading
+        if (GameController.Instance != null && GameController.Instance.GameOverCanvas != null)
+        {
+            GameController.Instance.GameOverCanvas.gameObject.SetActive(false);
+        }
+
+        // Clear checkpoint data before reloading
         if (CheckpointManager.Instance != null)
         {
             CheckpointManager.Instance.ClearCheckpointData();
         }
 
+        // Reload the scene to fully reset
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
+

ddaf45a5dffd4a89243e66fe4ab83f9755afa931
joann <j29bisseling@gmail.com>
2025-03-30 23:37:32 +0800

    props - jo

--- Assets/Scripts/Checkpoint/CPTorch.cs
+++ Assets/Scripts/Checkpoint/CPTorch.cs
@@ -1,42 +1,4 @@
-//using UnityEngine;
-
-//public class CPTorch : MonoBehaviour
-//{
-//    private Animator animator;
-//    private bool hasLit = false;
-
-//    private void Awake()
-//    {
-//        animator = GetComponent<Animator>();
-//    }
-
-//    private void Start()
-//    {
-//        // Ensure it always starts unlit
-//        ResetTorch();
-//    }
-
-//    public void TriggerLightup()
-//    {
-//        if (!hasLit)
-//        {
-//            hasLit = true;
-//            animator.Play("lightup_Front", 0, 0f);
-//        }
-//    }
-
-//    public void ResetTorch()
-//    {
-//        hasLit = false;
-//        if (animator != null)
-//        {
-//            animator.Play("unlit_Front", 0, 0f); // Reset to unlit idle
-//        }
-//    }
-//}
-
-
-// with lighting
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
@@ -62,44 +24,31 @@ public class CPTorch : MonoBehaviour
     {
         ResetTorch();
     }
-
-
-
-    //public void TriggerLightup()
-    //{
-    //    if (!hasLit)
-    //    {
-    //        hasLit = true;
-    //        animator.Play("lightup_Front", 0, 0f);
-
-    //        if (torchLight != null)
-    //        {
-    //            torchLight.enabled = true;
-    //        }
-    //        else
-    //        {
-    //            Debug.LogWarning("[CPTorch] No Light2D found under 'Light' child!");
-    //        }
-    //    }
-    //}
-
     public void TriggerLightup()
     {
         if (!hasLit)
         {
             hasLit = true;
-            Debug.Log("[CP Torch] Triggering light-up animation");  // <-- Debug line
+            Debug.Log("[CP Torch] Triggering light-up animation");
+
             animator.Play("lightup_Front", 0, 0f);
-            if (torchLight != null)
-            {
-                torchLight.enabled = true;
-            }
-            else
-            {
-                Debug.LogWarning("[CPTorch] No Light2D found under 'Light' child!");
-            }
+
+            // Start coroutine to enable light after animation finishes
+            StartCoroutine(EnableLightAfterDelay(animator.GetCurrentAnimatorStateInfo(0).length));
         }
     }
+
+    private IEnumerator EnableLightAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (torchLight != null)
+        {
+            torchLight.enabled = true;
+            Debug.Log("[CP Torch] Light enabled after animation");
+        }
+    }
+
 
 
     public void ResetTorch()
